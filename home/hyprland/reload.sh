eb14f938df9d5e8e8b047b7422b173492c4dbccf
JamisonHubbard <jhubbard956@gmail.com>
2024-06-01 00:07:56 -0600

    Added onto hyprland reload script

--- home/hyprland/reload.sh
+++ home/hyprland/reload.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
+killall swww-daemon
+swww init
+swww img /home/jamison/nixos/wallpapers/current
+
+killall .nm-applet-wrapped
+nm-applet --indicator
+
+killall .dunst-wrapped
+dunst
+
 killall .waybar-wrapped
 waybar &
