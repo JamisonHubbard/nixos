a1e8bbb657d245aaf34c5784e254fd035bc767ba
JamisonHubbard <jhubbard956@gmail.com>
2024-08-06 22:12:59 -0600

    Reinstalling NixOS on desktop

--- hardware/desktop.nix
+++ hardware/desktop.nix
@@ -1,69 +1,53 @@
 {
-    config,
-    lib,
-    modulesPath,
-    ...
-}: {
-    imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+  config,
+  lib, pkgs, modulesPath, ... }:
 
-    boot.initrd.availableKernelModules = [
-        "xhci_pci"
-        "ahci"
-        "nvme"
-        "usb_storage"
-        "usbhid"
-        "sd_mod"
-    ];
-    boot.initrd.kernelModules = [];
-    boot.kernelModules = [
-        "kvm-intel"
-        "nvidia-drm.modeset=1"
-        "nvidia-drm.fbdev=1"
-    ];
-    boot.extraModulePackages = [];
-
-    boot.loader.grub = {
-        enable = true;
-        efiSupport = true;
-        device = "nodev";
-        useOSProber = true;
-    };
-    boot.loader.efi.canTouchEfiVariables = true;
-
-    services.xserver.videoDrivers = [ "nvidia" ];
-    hardware.nvidia.modesetting.enable = true;
-    hardware.nvidia.prime = {
-        sync.enable = true;
-        amdgpuBusId = "PCI:0:0:0";
-        nvidiaBusId = "PCI:1:0:0";
-    };
-
-    fileSystems."/" = {
-        device = "/dev/disk/by-uuid/34663895-c618-4dec-8672-27c21fd819f9";
-        fsType = "ext4";
-    };
-
-    fileSystems."/boot" = {
-        device = "/dev/disk/by-uuid/7CAC-D7C3";
-        fsType = "vfat";
-        options = [
-            "fmask=0022"
-            "dmask=0022"
-        ];
-    };
-
-    swapDevices = [
-        { device = "/dev/disk/by-uuid/d7919bf4-e285-4091-a30d-e50f8cabdc7a"; }
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-# Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-# (the default) this is the recommended approach. When using systemd-networkd it's
-# still possible to use this option, but it's recommended to use it in conjunction
-# with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-    networking.useDHCP = lib.mkDefault true;
-# networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
-# networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" "nvidia-drm.modeset=1" "nvidia-drm.fbdev=1" ];
+  boot.extraModulePackages = [ ];
+ 
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
 
-    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.prime = {
+    sync.enable = true;
+    amdgpuBusId = "PCI:0:0:0";
+    nvidiaBusId = "PCI:1:0:0";
+  };
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/764e403a-3e87-4b18-9cb2-a04933a97d58";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/5522-1A1C";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/24a3d7bb-a5f0-465a-ba40-8bddfb40f758"; }
+    ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
